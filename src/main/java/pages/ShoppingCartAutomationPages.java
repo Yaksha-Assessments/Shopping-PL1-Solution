3823dfc1aaa46dc339cf62027ce5d43a4d494c41
Yaksha-Assessments <yaksha.mcq@gmail.com>
2025-03-21 16:04:14 +0530

    Update ShoppingCartAutomationPages.java

--- src/main/java/pages/ShoppingCartAutomationPages.java
+++ src/main/java/pages/ShoppingCartAutomationPages.java
@@ -28,7 +28,7 @@ public class ShoppingCartAutomationPages extends StartupPage
 	 * about this method verifyNavigateTotTheShoppingCartpage() 
 	 * @param : null
 	 * @description : go to this shopping cart page URL and validate the home page
-	 * @return : return String if page title is matching
+	 * @return : return the current page tile as String
 	 * @author : Yaksha
 	 */
 	public String verifyNavigateTotTheShoppingCartpage() throws Exception {
@@ -45,7 +45,7 @@ public class ShoppingCartAutomationPages extends StartupPage
 	 * about this method verifyShopOptionIsPresent() 
 	 * @param : null
 	 * @description : Check shop menu option is present or not
-	 * @return : return true if shopOption is present, else false
+	 * @return : return true if shop option is present, else false
 	 * @author : Yaksha
 	 */
 	public boolean verifyShopOptionIsPresent() throws Exception {
@@ -64,7 +64,7 @@ public class ShoppingCartAutomationPages extends StartupPage
 	 * about this method clickOnShopMenuandValidateTheTitle() 
 	 * @param : null
 	 * @description : click on the shop menu and validate the title of this shop page
-	 * @return : return String if page shop page title is matching
+	 * @return : return the current page tile as String
 	 * @author : Yaksha
 	 */
 	public String clickOnShopMenuandValidateTheTitle() throws Exception {
@@ -85,7 +85,7 @@ public class ShoppingCartAutomationPages extends StartupPage
 	 * @param : null
 	 * @description : validate the logo is present and click on the Automation practice site logo which is present at the left top corner
 	 * 				  and verify the page title.
-	 * @return : return String if page title is matching
+	 * @return : return the current page tile as String
 	 * @author : Yaksha
 	 */
 	public String clickOnLogoandVerifyItIsNavigateToTheHomePage() throws Exception {
@@ -126,7 +126,7 @@ public class ShoppingCartAutomationPages extends StartupPage
 	/**@Test6
 	 * about this method checkOfArrivalsSectionhasExactly3ProductsEnlisted() : Check of “Arrivals” section has exactly 3 products enlisted
 	 * @param : null
-	 * @description : Check of “Arrivals” section has exactly 3 products enlisted
+	 * @description : Check “New Arrivals” section has exactly 3 products enlisted
 	 * @return : return true if all three products are  present, else false
 	 * @author : Yaksha
 	 */
@@ -147,8 +147,8 @@ public class ShoppingCartAutomationPages extends StartupPage
 	/**@Test7
 	 * about this method clickOnSeleniumRubyProductAndValidatetheTitleOfThisPage() :Click on Selenium Ruby Product and validate the title of the page
 	 * @param : null
-	 * @description : Check of “Arrivals” section has exactly 3 products enlisted
-	 * @return : return String if seleniumRuby PageTitle is matching
+	 * @description : Verify that clicking the "Selenium Ruby" navigates to its product description page.
+	 * @return : return the current page tile as String
 	 * @author : Yaksha
 	 */
 	public String clickOnSeleniumRubyProductAndValidatetheTitleOfThisPage() throws Exception {
@@ -168,7 +168,7 @@ public class ShoppingCartAutomationPages extends StartupPage
 	 * about this method verifyAddBasketButtonIsPresentAndClickOnAddBasket() : Check product details page has an option ADD TO BASKET and Click on ADD TO BASKET
 	 * @param : null
 	 * @description : verify add basket button is present then click on the Add to basket button
-	 * @return : return true if viewBasketButton is  present, else false
+	 * @return : return true if "Add to Basket" Button is  present, else false
 	 * @author : Yaksha
 	 */
 	public boolean verifyAddBasketButtonIsPresentAndClickOnAddBasket() throws Exception {
@@ -189,7 +189,7 @@ public class ShoppingCartAutomationPages extends StartupPage
 	 * about this method viewBasketButtonIsPresentAndClickOnIt() : Verify that view basket button is present after click on add to basket and click on View basket
 	 * @param : null
 	 * @description : verify view basket button is present then click on the view basket button
-	 * @return : return true if seleniumRuby product is present InsideTheBasketPage, else false
+	 * @return : return true if "view Basket" Button is  present, else false
 	 * @author : Yaksha
 	 */
 	public boolean viewBasketButtonIsPresentAndClickOnIt() throws Exception {
@@ -210,7 +210,7 @@ public class ShoppingCartAutomationPages extends StartupPage
 	 * about this method userIsNavigateToTheBasketPage() 
 	 * @param : null
 	 * @description : verify the title of the Basket Page
-	 * @return : return String if  PageTitle is matching
+	 * @return : return the current page tile as String
 	 * @author : Yaksha
 	 */
 	public String userIsNavigateToTheBasketPage() throws Exception {
@@ -228,7 +228,7 @@ public class ShoppingCartAutomationPages extends StartupPage
 	 * about this method seleniumrubyIsPresentInsideTheCartPage() : Verify after adding selenium ruby product in to cart verify the selenium ruby product is present inside the Cart page.
 	 * @param : null
 	 * @description :seleniumruby Is PresentInside The CartPage
-	 * @return : return true if product seleniumRuby Inside The Basket Page  else false
+	 * @return : return true if product seleniumRuby Inside The Basket Page, else false
 	 * @author : Yaksha
 	 */
 	public Boolean seleniumrubyIsPresentInsideTheCartPage() throws Exception {
@@ -248,11 +248,11 @@ public class ShoppingCartAutomationPages extends StartupPage
 	/**@Test12
 	 * about this method verifythatSeleniumRubyProductPriceIs500() : Verify that selenium ruby product price is 500 in the Cart page
 	 * @param : null
-	 * @description : verify that SeleniumRuby Product Price Is 500
-	 * @return : return true if product price is displayed else false
+	 * @description : verify that SeleniumRuby Product Price Is Displayed
+	 * @return : return true if product price is displayed, else false
 	 * @author : Yaksha
 	 */
-	public Boolean verifythatSeleniumRubyProductPriceIs500() throws Exception {
+	public Boolean verifythatSeleniumRubyProductPriceIsDisplayed() throws Exception {
 		Boolean seleniumRubyProductPriceIsDisplayed=false;
 		try {
 			if(commonEvents.isDisplayed(seleniumRubyProductPrice))
